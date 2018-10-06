cf948e375582ae5a4531c19e9da3fa0a3c867c6f
Abdennebi Mohamed <abdennebi@gmail.com>
2018-10-06 11:04:33 +0200

    Delete scripts folder

--- scripts/install-recommendations-v2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-oc apply -f <(istioctl kube-inject --debug -f ../recommendation/src/main/kubernetes/Deployment-v2.yml)
--- scripts/poll_customer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-URL="$CUTOMER_URL"
-
-while true
-do curl $URL
-sleep .1
-done
-
