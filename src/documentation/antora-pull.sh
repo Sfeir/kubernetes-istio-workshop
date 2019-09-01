a652a04d9173a353194fbeaf020baab094087f42
Mohamed Abdennebi <abdennebi.m@sfeir.com>
2019-09-01 13:21:14 +0200

    Set the  Antora version number

--- src/documentation/antora-pull.sh
+++ src/documentation/antora-pull.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -ti --rm --name antora -v $(pwd):/repo -w /repo/src/documentation antora/antora --pull site.yml
+docker run -ti --rm --name antora -v $(pwd):/repo -w /repo/src/documentation antora/antora:1.1.1 --pull site.yml  --stacktrace
