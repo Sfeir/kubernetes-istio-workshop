72bfe7fc9b1d1dacba21682fd35c35ee5a4cde72
Julien Furgerot <julien.furgerot@gmail.com>
2019-10-23 00:55:20 +0200

    force add .nojekyll file to avoid gh-pages rendering

--- generate_site.sh
+++ generate_site.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker run -ti --rm --name antora -v $(pwd):/repo -w /repo/src/documentation --network host antora/antora:1.1.1 --pull site.yml  --stacktrace
+docker run -ti --rm --name antora \
+    -v $(pwd):/repo -w /repo/src/documentation antora/antora:1.1.1 \
+    --clean --pull site.yml --stacktrace
+
+touch docs/.nojekyll
+touch docs/_/.nojekyll
